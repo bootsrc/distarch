22a68ff2deefe101304f18ff5789ce881ecbf5fe
flylib <liushaomingdev@163.com>
2019-01-11 19:11:09 +0800

    set password

---
+++ sword-jdbc/src/main/java/com/appjishu/jdbc/config/DataSourceConfigConst.java
@@ -0,0 +1,11 @@
+package com.appjishu.jdbc.config;
+
+public class DataSourceConfigConst {
+    public static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/mall?serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=utf8&useSSL=false";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "yourPassword";
+
+
+
+}
